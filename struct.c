88abfcfcc85a95b067872f12dcb1f78f1b0edfcb
Clh___ <100593454+reculejtencule@users.noreply.github.com>
2022-10-28 09:19:00 +0200

    tree structure

---
+++ struct.c
@@ -0,0 +1,5 @@
+//
+// Created by Coelho Arthur on 28/10/2022.
+//
+
+#include "struct.h"
---
+++ struct.h
@@ -0,0 +1,28 @@
+#ifndef WORDGENERATOR_STRUCT_H
+#define WORDGENERATOR_STRUCT_H
+
+
+typedef struct node {
+    char data;
+    struct list* child;
+}Node;
+
+
+typedef struct cell {
+    Node* data;
+    struct cell* next;
+}Cell;
+
+
+typedef struct list {
+    Cell* head;
+}List;
+
+
+typedef struct tree {
+    Node* root;
+}Tree;
+
+
+
+#endif
